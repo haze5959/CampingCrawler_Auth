f2259d2fb741c9d8e9465a295652ed0335a8b4c4
KwonOQ <haze5959@naver.com>
2021-04-28 01:20:28 +0900

    dsfsx

--- auth.py
+++ auth.py
@@ -16,46 +16,52 @@ def get_user():
     if 'token' in request.args:
         id_token = request.args['token']
     else:
-        return "Error: No token field provided. Please specify an token."
+        print("Error: No token field provided. Please specify an token.")
+        return jsonify({'result': False, 'msg': "No token"})
 
     try:
         decoded_token = auth.verify_id_token(id_token)
     except firebase_admin._auth_utils.InvalidIdTokenError:
-        return "Error: ID Token is invalid."
+        print("Error: ID Token is invalid.")
+        return jsonify({'result': False, 'msg': "ID Token is invalid"})
 
     uid = decoded_token['uid']
 
     try:
         user = auth.get_user(uid)
     except:
-        return "Error: Auth fail."
+        print("Error: Auth fail.")
+        return jsonify({'result': False, 'msg': "Auth fail"})
 
     print('Successfully fetched user data: {0}'.format(user.uid))
 
-    return jsonify(user)
+    return jsonify({'result': True, 'data': user})
 
 @app.route('/', methods=['DELETE'])
 def delete_user():
     if 'token' in request.args:
         id_token = request.args['token']
     else:
-        return "Error: No token field provided. Please specify an token."
+        print("Error: No token field provided. Please specify an token.")
+        return jsonify({'result': False, 'msg': "No token"})
 
     try:
         decoded_token = auth.verify_id_token(id_token)
     except firebase_admin._auth_utils.InvalidIdTokenError:
-        return "Error: ID Token is invalid."
+        print("Error: ID Token is invalid.")
+        return jsonify({'result': False, 'msg': "ID Token is invalid"})
 
     uid = decoded_token['uid']
 
     try:
         auth.delete_user(uid)
     except:
-        return "Error: Auth fail."
+        print("Error: Auth fail.")
+        return jsonify({'result': False, 'msg': "Auth fail"})
 
     print('Successfully deleted user')
 
-    return {result: True}
+    return jsonify({'result': True, })
 
 
 app.run()
