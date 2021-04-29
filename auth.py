09470c078a65f32fbcd13d0edc0b308fa0b1ac37
OGyu kwon <haze5959@gmail.com>
2021-04-29 11:59:07 +0900

    13131313

--- auth.py
+++ auth.py
@@ -1,67 +1,64 @@
-import flask
-from flask import request, jsonify
+from typing import Optional
+from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import auth
 from firebase_admin import credentials
+import uvicorn
 
 cred = credentials.Certificate("./good-place-camp-accountKey.json")
 firebase_admin.initialize_app(cred)
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = FastAPI()
 
 
-@app.route('/', methods=['GET'])
-def get_user():
-    if 'token' in request.args:
-        id_token = request.args['token']
-    else:
-        print("Error: No token field provided. Please specify an token.")
-        return jsonify({'result': False, 'msg': "No token"})
+@app.get("/{token}")
+def get_user(token: str):
+    if token is None:
+        # print("Error: No token field provided. Please specify an token.")
+        return {'result': False, 'msg': "No token"}
 
     try:
-        decoded_token = auth.verify_id_token(id_token)
+        decoded_token = auth.verify_id_token(token)
     except firebase_admin._auth_utils.InvalidIdTokenError:
-        print("Error: ID Token is invalid.")
-        return jsonify({'result': False, 'msg': "ID Token is invalid"})
+        # print("Error: ID Token is invalid.")
+        return {'result': False, 'msg': "ID Token is invalid"}
 
     uid = decoded_token['uid']
 
     try:
-        user = auth.get_user(uid)
+        data = auth.get_user(uid)._data
     except:
-        print("Error: Auth fail.")
-        return jsonify({'result': False, 'msg': "Auth fail"})
+        # print("Error: Auth fail.")
+        return {'result': False, 'msg': "Auth fail"}
 
-    print('Successfully fetched user data: {0}'.format(user.uid))
+    # print('Successfully fetched user data: {0}'.format(user.uid))
 
-    return jsonify({'result': True, 'data': user})
+    return {'result': True, 'data': data}
 
-@app.route('/', methods=['DELETE'])
-def delete_user():
-    if 'token' in request.args:
-        id_token = request.args['token']
-    else:
-        print("Error: No token field provided. Please specify an token.")
-        return jsonify({'result': False, 'msg': "No token"})
+
+@app.delete("/{token}")
+def delete_user(token: str):
+    if token is None:
+        # print("Error: No token field provided. Please specify an token.")
+        return {'result': False, 'msg': "No token"}
 
     try:
-        decoded_token = auth.verify_id_token(id_token)
+        decoded_token = auth.verify_id_token(token)
     except firebase_admin._auth_utils.InvalidIdTokenError:
-        print("Error: ID Token is invalid.")
-        return jsonify({'result': False, 'msg': "ID Token is invalid"})
+        # print("Error: ID Token is invalid.")
+        return {'result': False, 'msg': "ID Token is invalid"}
 
     uid = decoded_token['uid']
 
     try:
         auth.delete_user(uid)
     except:
-        print("Error: Auth fail.")
-        return jsonify({'result': False, 'msg': "Auth fail"})
+        # print("Error: Auth fail.")
+        return {'result': False, 'msg': "Auth fail"}
 
-    print('Successfully deleted user')
+    # print('Successfully deleted user')
 
-    return jsonify({'result': True, })
+    return {'result': True, }
 
 
-app.run()
+if __name__ == "__main__":
+    uvicorn.run(app, port=5000)
